0c977407d7540d29291e518da95685c788fc9575
XavierSchiller <22575255+XavierSchiller@users.noreply.github.com>
2019-12-21 18:12:02 +0530

    Error In Redirection

--- InstallScript.sh
+++ InstallScript.sh
@@ -3,9 +3,9 @@ Node=1
 
 echo "Dev Installer Running... "
 
-if ! type "$curl" &> /dev/null then
+if ! type "$curl" &> /dev/null; then
     echo "Installing Curl..."
-    apt install curl -y 1&2> /dev/null
+    apt install curl -y &> /dev/null
 else
     echo "Curl Has Already Been Installed"
 fi
@@ -13,9 +13,9 @@ fi
 if [ Node -eq 0 ]; then
     echo "Installing node"
     curl -sL https://deb.nodesource.com/setup_12.x -o nodesource_setup.sh
-    bash nodesource_setup.sh > /dev/null
-    apt-get -qq update > /dev/null
-    apt-get install nodejs -y > /dev/null
+    bash nodesource_setup.sh &> /dev/null
+    apt-get -qq update &> /dev/null
+    apt-get install nodejs -y &> /dev/null
     echo "Nodejs Has been Installed." 
 else
     echo "Skipping Installation of Node"
