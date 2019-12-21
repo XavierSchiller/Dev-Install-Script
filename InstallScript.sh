0554db7a410fe71b5697cb628c5f98ff0676908d
XavierSchiller <22575255+XavierSchiller@users.noreply.github.com>
2019-12-21 17:47:20 +0530

    Adding curl installation.

--- InstallScript.sh
+++ InstallScript.sh
@@ -1,8 +1,20 @@
-### Installing Node
-cd ~
-curl -sL https://deb.nodesource.com/setup_12.x -o nodesource_setup.sh
-sudo bash nodesource_setup.sh
-sudo apt-get update
-sudo apt-get install nodejs
+Node=1
 
-echo "Nodejs Has been Installed."
+
+echo "Dev Installer Running... "
+echo "Installing Curl"
+
+apt install curl
+
+if [Node -eq 0 ] then
+    echo "Installing node"
+    curl -sL https://deb.nodesource.com/setup_12.x -o nodesource_setup.sh
+    bash nodesource_setup.sh > /dev/null
+    apt-get -qq update > /dev/null
+    apt-get install nodejs -y > /dev/null
+    echo "Nodejs Has been Installed."   
+
+else
+    echo "Skipping Installation of Node"
+
+fi
