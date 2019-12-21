ab7b7a41070d5c4a5a19df75e40d9d7ad8cb070e
XavierSchiller <22575255+XavierSchiller@users.noreply.github.com>
2019-12-21 16:43:15 +0530

    Nodejs added

---
+++ InstallScript.sh
@@ -0,0 +1,8 @@
+### Installing Node
+cd ~
+curl -sL https://deb.nodesource.com/setup_12.x -o nodesource_setup.sh
+sudo bash nodesource_setup.sh
+sudo apt-get update
+sudo apt-get install nodejs
+
+echo "Nodejs Has been Installed."
