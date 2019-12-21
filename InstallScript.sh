1f25e1d327f148149238835406bd9c845ac5d96c
XavierSchiller <22575255+XavierSchiller@users.noreply.github.com>
2019-12-21 17:57:19 +0530

    Silly Errors

--- InstallScript.sh
+++ InstallScript.sh
@@ -6,15 +6,13 @@ echo "Installing Curl"
 
 apt install curl -y > /dev/null
 
-if [Node -eq 0 ] then
+if [Node -eq 0 ]; then
     echo "Installing node"
     curl -sL https://deb.nodesource.com/setup_12.x -o nodesource_setup.sh
     bash nodesource_setup.sh > /dev/null
     apt-get -qq update > /dev/null
     apt-get install nodejs -y > /dev/null
-    echo "Nodejs Has been Installed."   
-
+    echo "Nodejs Has been Installed." 
 else
     echo "Skipping Installation of Node"
-
 fi
