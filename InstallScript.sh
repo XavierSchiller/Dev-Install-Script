fbed2a87235cfd427b27e7bcf126ad7e3c1765dd
XavierSchiller <22575255+XavierSchiller@users.noreply.github.com>
2019-12-21 18:27:03 +0530

    Some Textual Corrections

--- InstallScript.sh
+++ InstallScript.sh
@@ -7,16 +7,16 @@ if ! type "$curl" &> /dev/null; then
     echo "Installing Curl..."
     apt install curl -y &> /dev/null
 else
-    echo "Curl Has Already Been Installed"
+    echo "Curl Has Already Been Installed!"
 fi
 
 if [ $Node -eq 0 ]; then
-    echo "Installing node"
+    echo "Installing Node 12 along with Npm..."
     curl -sL https://deb.nodesource.com/setup_12.x -o nodesource_setup.sh
-    bash nodesource_setup.sh
-    apt-get -qq update
-    apt-get install nodejs -y 
-    echo "Nodejs Has been Installed." 
+    bash nodesource_setup.sh &> /dev/null
+    apt-get -qq update &> /dev/null
+    apt-get install nodejs npm -y &> /dev/null
+    echo "Nodejs and npm has been Installed!" 
 else
     echo "Skipping Installation of Node"
 fi
